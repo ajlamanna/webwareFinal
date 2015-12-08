4c4dacb487610931b1e62f4ced3def107a88bd99
ajlamanna <ajlamanna13@gmail.com>
2015-12-08 18:56:16 -0500

    puzzle three

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var fs = require("fs");
-var cookie = require('cookie-parser');
+var cookie = require('cookie');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -15,6 +15,18 @@ app.use(bodyParser.json({
   extended: true
 }));
 
+function parseCookies (request) {
+  var list = {},
+      rc = request.headers.cookie;
+
+  rc && rc.split(';').forEach(function( cookie ) {
+    var parts = cookie.split('=');
+    list[parts.shift().trim()] = decodeURI(parts.join('='));
+  });
+
+  return list;
+}
+
 app.get('/', function(req, res) {
   res.writeHead(200, {"Content-Type": "text/html"})
 });
@@ -25,16 +37,19 @@ app.post('/', function(req, res) {
 
 app.get('/index', function(req, res) {
   res.writeHead(200, {"Content-Type": "text/html"})
+  console.log(req.cookie.password);
 });
 
 app.post('/index', function(req, res) {
   res.writeHead(200, {"Content-Type": "text/html"})
 });
 
-app.get('/cookie', function(req, res) {
-  if(req.cookie.password.toLowerCase() === "level4" || req.cookie.password.toLowerCase() === "level 4"){
-    res.redirect('littlegame.herokuapp.com/color.html');
+app.post('/cookie', function(req, res) {
+  //res.writeHead(200, {"Content-Type": "text/html"});
+  var cookies = parseCookies(req);
+  if(cookies.password.toLowerCase() === "level4" || cookies.password.toLowerCase() === "level 4") {
     res.clearCookie('password');
+    res.redirect('littlegame.herokuapp.com/color.html');
   }
 });
 
@@ -46,16 +61,18 @@ app.listen(port, function() {
 // subroutines
 
 function sendFile(res, filename) {
-  res.writeHead(200, {'Content-type': 'text/html'})
+  res.writeHead(200, {'Content-type': 'text/html'});
 
-  var stream = fs.createReadStream(filename)
+  var stream = fs.createReadStream(filename);
 
   stream.on('data', function(data) {
     res.write(data);
-  })
+  });
 
   stream.on('end', function(data) {
     res.end();
     return;
-  })
+  });
+
+
 }
